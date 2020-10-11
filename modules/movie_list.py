d629c91dc3cbbdf2a3fefdbc92828d09bbd8211d
Róbert Vári <mail.robertvari@gmail.com>
2020-10-11 16:43:37 +0200

    MovieListWorker and signals

--- modules/movie_list.py
+++ modules/movie_list.py
@@ -1,4 +1,6 @@
-from PySide2.QtCore import QAbstractListModel, QModelIndex, Qt
+from PySide2.QtCore import QAbstractListModel, QModelIndex, \
+    Qt, QRunnable, QObject, QThreadPool, Signal
+import time
 
 
 class MovieList(QAbstractListModel):
@@ -8,19 +10,13 @@ class MovieList(QAbstractListModel):
         super(MovieList, self).__init__()
         self.items = []
         self.current_page = 1
+        self.pool = QThreadPool()
+        self.pool.setMaxThreadCount(1)
 
         self._fetch()
 
     def _fetch(self):
-        for i in range(20):
-            movie_data = {
-                "movie_id": i,
-                "original_title": "Star Wars",
-                "poster_path": "../images/vfrQk5IPloGg1v9Rzbh2Eg3VGyM.jpg",
-                "popularity": 80,
-                "release_date": "1977-05-25"
-            }
-            self.insert_movie(movie_data)
+        pass
 
     def insert_movie(self, movie_data):
         self.beginInsertRows(QModelIndex(), self.rowCount(), self.rowCount())
@@ -45,3 +41,31 @@ class MovieList(QAbstractListModel):
         return {
             MovieList.DataRole: b'movie_item'
         }
+
+
+class WorkerSignals(QObject):
+    finished = Signal(dict)
+
+    def __init__(self):
+        super(WorkerSignals, self).__init__()
+
+
+class MovieListWorker(QRunnable):
+    def __init__(self):
+        super(MovieListWorker, self).__init__()
+        self.signals = WorkerSignals()
+
+    def run(self):
+        print("Downloading movie data....")
+        for i in range(20):
+            time.sleep(1)
+            movie_data = {
+                "movie_id": 10,
+                "original_title": "Star Wars",
+                "poster_path": "../images/vfrQk5IPloGg1v9Rzbh2Eg3VGyM.jpg",
+                "popularity": 80,
+                "release_date": "1977-05-25"
+            }
+
+            print("Finished!!")
+            self.signals.finished.emit(movie_data)
